e09d7439fe4de9522be1c32fa8ae56869824c27f
sehas <sehassaini@gmail.com>
2018-10-31 19:18:36 -0700

    add stattement

--- src/main/java/nl/sogyo/boilerplate/service/AccountService.java
+++ src/main/java/nl/sogyo/boilerplate/service/AccountService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import nl.sogyo.boilerplate.domain.SavingsAccount;
 
 public interface AccountService {
+//checking by me 10/31/2018 
 
 	public List<SavingsAccount> getSavingsAccounts();
 	public SavingsAccount getSavingsAccount(int accountId);
