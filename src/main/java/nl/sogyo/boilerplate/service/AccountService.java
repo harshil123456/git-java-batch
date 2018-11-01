78cfbeb08f033862e0b407ce49e34ce1ef3a0ea8
ankita mohanty <mohanty.vethankita33@gmail.com>
2018-10-31 19:40:29 -0700

    Hey, I changed the input type

--- src/main/java/nl/sogyo/boilerplate/service/AccountService.java
+++ src/main/java/nl/sogyo/boilerplate/service/AccountService.java
@@ -9,5 +9,5 @@ public interface AccountService {
 	public List<SavingsAccount> getSavingsAccounts();
 	public SavingsAccount getSavingsAccount(int accountId);
 	public void createSavingsAccount(SavingsAccount savingsAccount);
-	public void updateSavingsAccount(int accountId, SavingsAccount savingsAccount);
+	public void updateSavingsAccount(String accountId, SavingsAccount savingsAccount);
 }
